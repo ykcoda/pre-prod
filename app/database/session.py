a9d87e7c95668a1cee2627ea9c84cef3316b2926
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-19 12:50:22 +0100

    Add a database asynchronous session

---
+++ app/database/session.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.config import db_settings
+
+
+engine = create_async_engine(url=db_settings.DATABASE_URL, echo=True)
+
+
+async def get_database_session():
+    async_session = sessionmaker(
+        bind=engine, class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with async_session() as session:
+        yield session
