2d4db3284e17a2ca1d01bd0d832c193eda475772
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-19 13:49:56 +0100

    Add max_handling_capacity and servicable_zip_code columns to the DeliveryPartner Model

--- app/database/models/delivery_partner.py
+++ app/database/models/delivery_partner.py
@@ -1,7 +1,8 @@
 from uuid import UUID, uuid4
 from datetime import datetime
 
-from sqlmodel import Field
+from sqlalchemy import ARRAY, INTEGER
+from sqlmodel import Field, Column
 
 from .user import User
 
@@ -11,4 +12,6 @@ class DeliverPartners(User):
 
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     password_hash: str
+    servicable_zip_codes: list[int] = Field(sa_column=Column(ARRAY(INTEGER)))
+    max_handling_capacity: int
     created_at: datetime = Field(default_factory=lambda: datetime.now())
