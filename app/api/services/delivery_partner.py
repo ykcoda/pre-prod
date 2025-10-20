f4dbb40a1391d0a048fa0c5632b16b354acec37d
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-20 15:10:07 +0100

    Add the get_delivery_partner_by_zipcode function to the DeliveryPartnerService

--- app/api/services/delivery_partner.py
+++ app/api/services/delivery_partner.py
@@ -1,5 +1,5 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
-
+from sqlmodel import select, any_
 from .user import UserService
 from app.database.models.delivery_partner import DeliveryPartners
 from app.database.schema.delivery_partner import DeliveryPartnerCreate
@@ -15,3 +15,11 @@ class DeliveryPartnerService(UserService):
 
     async def partner_token(self, email, password):
         return await self._get_user_token(email, password)
+
+    async def get_delivery_partner_by_zipcode(self, zipcode: int):
+        result = await self.session.exec(
+            select(DeliveryPartners).where(
+                zipcode == any_(DeliveryPartners.servicable_zip_codes)
+            )
+        )
+        return result.all()
