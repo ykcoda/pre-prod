f11eea5bec14d57d09507dee7f2d1710746b7afc
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-21 18:43:56 +0100

    Add shipment relationship to sellers and delivery_partners

--- app/database/models/delivery_partner.py
+++ app/database/models/delivery_partner.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from sqlmodel import ARRAY, INTEGER, Column, Field
+from sqlmodel import ARRAY, INTEGER, Column, Field, Relationship
 from uuid import UUID, uuid4
 from .user import User
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .shipment import Shipment
 
 
 class DeliveryPartner(User, table=True):
@@ -13,3 +17,7 @@ class DeliveryPartner(User, table=True):
     max_capacity: int
     servicable_zipcodes: list[int] = Field(sa_column=Column(ARRAY(INTEGER)))
     created_at: datetime = Field(default_factory=lambda: datetime.now())
+
+    shipments: list["Shipment"] = Relationship(
+        back_populates="partner", sa_relationship_kwargs={"lazy": "selectin"}
+    )
