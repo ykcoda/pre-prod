550fcc6eba7c4bcfb4741e36903fa1be2776b239
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-21 13:09:59 +0100

    Add the get_user_by_email and get_user_access_token function to the User Service

--- app/api/service/user.py
+++ app/api/service/user.py
@@ -1,8 +1,10 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
 from passlib.context import CryptContext  # type: ignore
-
+from sqlmodel import select
+from fastapi import HTTPException, status
 
 from app.database.models.user import User
+from app.utils import request_access_code
 
 from .base import BaseService
 
@@ -21,3 +23,21 @@ class UserService(BaseService):
         )  # type: ignore
 
         return await self._add(new_user)
+
+    async def get_user_by_email(self, email) -> User | None:
+        result = await self.session.exec(
+            select(self.model).where(self.model.email == email)  # type:ignore
+        )
+        return result.first()
+
+    async def get_user_access_token(self, email, password):
+        user = await self.get_user_by_email(email)
+
+        if user is None or not password_context.verify(password, user.hashed_password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized access"
+            )
+
+        data = {"user": {"email": user.email, "id": str(user.id)}}
+        
+        return request_access_code(data)
