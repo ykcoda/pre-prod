06e53e60568ab688f9c157d88e0f3cc28cfff15d
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-21 17:53:13 +0100

    Update master router with the partner router

--- app/api/routers/master.py
+++ app/api/routers/master.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .seller import seller
+from .delivery_partner import partner
 
 master = APIRouter()
 master.include_router(seller)
+master.include_router(partner)
