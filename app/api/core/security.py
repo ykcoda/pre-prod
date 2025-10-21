fe8197dd1bfc40d7ff50354f0df197a71e44574e
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-21 17:49:47 +0100

    Add Partner OAuthe Scheme

--- app/api/core/security.py
+++ app/api/core/security.py
@@ -2,3 +2,4 @@ from fastapi.security import OAuth2PasswordBearer
 
 
 SELLER_OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="/sellers/token")
+PARTNER_OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="/partners/token")
