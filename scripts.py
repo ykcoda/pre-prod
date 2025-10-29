7f0ef8ec5da4640a9f726062ad2171701fc08b39
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-29 18:06:55 +0000

    Explain OOP Dependency Inversion Principle: REFACTORED using Dependency Injection

--- scripts.py
+++ scripts.py
@@ -5,19 +5,33 @@ High-level modules should not depend on low-level modules. Both should
 depend pn abstractions.
 """
 
+from abc import ABC, abstractmethod
 
-class Engine:
+
+class Engine(ABC):
+    @abstractmethod
     def start(self):
-        print("Engine Started.")
+        pass
+
+
+class BasicEngine(Engine):
+    def start(self):
+        print("Basic Engine Started")
+
+
+class FastEngine(Engine):
+    def start(self):
+        print("Fast Engine Started")
 
 
 class Car:
-    def __init__(self):
-        self.engine = Engine()
+    def __init__(self, engine: Engine):
+        self.engine = engine
 
     def start(self):
         self.engine.start()
         print("Car Started")
-        
-car = Car()
+
+
+car = Car(BasicEngine())
 car.start()
