8d4a087dce051d111c0249832ec4c2ea6c21be2a
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-29 11:41:34 +0000

    Explain OOP Fragile Base Class Problem

--- scripts.py
+++ scripts.py
@@ -1,57 +1,14 @@
 """
-Composition
-This involves creating complex objects by combining simpler object or components.
+Fragile Base Class Problem
+Is a software design issue that where changes made to a base class
+can break the functionality of derived classes. this problem occures to the
+tight coupling between base and derived classes in inheritance hierarchies
+
+
+Key Points:
+1. Inheritance coupling
+2. Ripple Effect
+3. Limited Extensibility
+4. Brittle Software
+5. Mitigation Strategies
 """
-
-"""
-Composition vrs Inheritance
-
-
-Composition: 
-1. When you need more flexibity in constructing objects by assembling smalle, reusable components
-2. When there is no clear "is-a" relationship between classes and a "has-a" relationship is more appropriate
-3. When you want to avaoid the limitation of inheritance, such as tight coupling and the frigile base class problem
-
-Inheritance:
-1. When there is a clear "is-a" relationship between classes and subclasses objects can be treated as instances of their superclass.
-2. When you want to promote code reuse by inheriting properties and behaviours from existing classes
-"""
-
-
-class Engine:
-
-    def start(self):
-        print("Engine starting")
-
-
-class Wheels:
-    def rotates(self):
-        print("Rotate wheels")
-
-
-class Chassis:
-    def support(self):
-        print("Chassis is supporting car")
-
-
-class Seat:
-    def sit(self):
-        print("Sitting on seats")
-
-
-class Car:
-    def __init__(self):
-        self._engine = Engine()
-        self._wheels = Wheels()
-        self._chassis = Chassis()
-        self._seat = Seat()
-
-    def start(self):
-        self._engine.start()
-        self._wheels.rotates()
-        self._chassis.support()
-        self._seat.sit()
-
-
-car1 = Car()
-car1.start()
