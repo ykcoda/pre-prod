a370490b712d1482189011df62b00c7179525e9e
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-29 11:36:55 +0000

    Explain OOP Composition and also the difference between inheritance and composition

--- scripts.py
+++ scripts.py
@@ -1,42 +1,57 @@
 """
-Coupling
-The degree of dependencies between different classes or modules within a system
+Composition
+This involves creating complex objects by combining simpler object or components.
 """
 
-from abc import ABC, abstractmethod
+"""
+Composition vrs Inheritance
 
 
-class NotificationService(ABC):
-    @abstractmethod
-    def send_notification(self, message: str):
-        pass
+Composition: 
+1. When you need more flexibity in constructing objects by assembling smalle, reusable components
+2. When there is no clear "is-a" relationship between classes and a "has-a" relationship is more appropriate
+3. When you want to avaoid the limitation of inheritance, such as tight coupling and the frigile base class problem
+
+Inheritance:
+1. When there is a clear "is-a" relationship between classes and subclasses objects can be treated as instances of their superclass.
+2. When you want to promote code reuse by inheriting properties and behaviours from existing classes
+"""
 
 
-class EmailService(NotificationService):
-    def send_notification(self, message):
-        print(f"Sending email: {message}")
+class Engine:
+
+    def start(self):
+        print("Engine starting")
 
 
-class MobileService(NotificationService):
-    def send_notification(self, message):
-        print(f"Sending text message: {message}")
+class Wheels:
+    def rotates(self):
+        print("Rotate wheels")
 
 
-class Order:
-
-    def __init__(self, notification_service: NotificationService):
-        self.notification_service = notification_service
-
-    def create(self):
-        # Perform order creating logic, validate order details,
-        # check product stock and save to database...
-        self.notification_service.send_notification(
-            "Hi, your order was placed successfuly and will be delivered within 2-5 working days."
-        )
+class Chassis:
+    def support(self):
+        print("Chassis is supporting car")
 
 
-order1 = Order(MobileService())
-order1.create()
+class Seat:
+    def sit(self):
+        print("Sitting on seats")
 
-order2 = Order(EmailService())
-order2.create()
+
+class Car:
+    def __init__(self):
+        self._engine = Engine()
+        self._wheels = Wheels()
+        self._chassis = Chassis()
+        self._seat = Seat()
+
+    def start(self):
+        self._engine.start()
+        self._wheels.rotates()
+        self._chassis.support()
+        self._seat.sit()
+
+
+car1 = Car()
+car1.start()
