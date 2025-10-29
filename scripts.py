2f946241de348cf55977dbc8843f70da8ed8e6a8
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-29 19:14:36 +0000

    Explain OOP Design Pattern: State Patten: naive solution

--- scripts.py
+++ scripts.py
@@ -3,6 +3,8 @@ Behavioral: The interaction or communication between those objects
 """
 
 from enum import Enum
+from abc import ABC, abstractmethod
+from scripts import State
 
 
 class DocumentState(Enum):
@@ -17,25 +19,40 @@ class UserRoles(Enum):
     ADMIN = 3
 
 
+class State(ABC):
+    @abstractmethod
+    def publish(self):
+        pass
+
+
+class DraftState(State):
+    def __init__(self, document):
+        self._document = document
+
+    # def publish(self):
+    #     self._document.State = 
+
+
 class Document:
 
-    def __init__(self, state: DocumentState, current_user_role: UserRoles):
-        self.state = state
+    def __init__(self, current_user_role: UserRoles):
+        self.state = DraftState(self)
         self.current_user_role = current_user_role
 
     def publish(self):
-        if self.state == DocumentState.DRAFT:
-            self.state = DocumentState.MODERATION
+        self.state.publish()
+        # if self.state == DocumentState.DRAFT:
+        #     self.state = DocumentState.MODERATION
 
-        elif (
-            self.state == DocumentState.MODERATION
-            and self.current_user_role == UserRoles.ADMIN
-        ):
-            self.state = DocumentState.PUBLISHED
+        # elif (
+        #     self.state == DocumentState.MODERATION
+        #     and self.current_user_role == UserRoles.ADMIN
+        # ):
+        #     self.state = DocumentState.PUBLISHED
 
-        elif self.state == DocumentState.PUBLISHED:
-            # Do nothing
-            pass
+        # elif self.state == DocumentState.PUBLISHED:
+        #     # Do nothing
+        #     pass
 
 
 doc = Document(DocumentState.DRAFT, UserRoles.EDITOR)
