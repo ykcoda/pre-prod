0aa879aaf875643f84f25535c233e5c864a14c43
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-29 12:28:47 +0000

    Explain OOP Single Responsibility Principles

--- scripts.py
+++ scripts.py
@@ -1,11 +1,25 @@
 """
-The solid principle are 5 software design principles that help you 
-to write high quality, flexible, maitainable, reusable, testable and readable software 
-
-S: Single Responsibility  (SRP)
-O: Open-Closed Principle (OCP)
-L: Liskov Substitution Principle (LSP)
-I: Interface Segregation Principle (ISP)
-D: Dependency Inversion Principle (DIP)
+S: Single Responsibility Principle (SRP)
+A class should have only one reason to change, meaning that it should have only one
+responsibility or purpose
 """
 
+class EmailSender:
+    def send(self, recipient, message):
+        print(f"Sending email to {recipient}: {message}")
+
+
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def register(self):
+        print(f"Registring user {self.username}")
+
+        email_sender = EmailSender()
+        email_sender.send(self.email, f"Welcome to our platform {self.username}!")
+
+
+user1 = User("YK", "yk@gmail.com")
+user1.register()
