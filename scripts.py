d972f98935e469f18f227a9831110c9d3fd3b45e
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-10-29 18:24:10 +0000

    Explain OOP Design Pattern

--- scripts.py
+++ scripts.py
@@ -1,10 +1,25 @@
-"""
-Dependency Injection (DI) offers several advantages in software development
+""" 
+Software Design Patterns
 
-1. Decoupling
-2. Testability
-3. Flexibility
-4. Readability and Maitainability
-5. Reusability
-6. Scalability
+Design patterns are proven, reusable solutions to common ploblems in 
+software design, providing templates or guidlines for structuring code 
+to solve recurring challenges in a consistent and efficient way.
+
+Why Design Patterns? 
+
+1. Reusable Solutions
+2. Standardized Terminology
+3. Scalability 
+4. Maintainability
+5. Performance
+6. Documentation
+7. Best Practices 
+"""
+""" 
+Gang of Four (GoF) Design Patterns
+
+1. Creational: The different ways to create objects
+2. Structural: The relationship between those objects
+3. Behavioral: The interaction or communication between those objects 
+
 """
